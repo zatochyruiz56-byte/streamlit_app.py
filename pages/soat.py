352bab04d7a9fd160d93cdc1bfeb5578057d0325
zatochyruiz56-byte <zatochyruiz56@gmail.com>
2026-02-03 20:23:48 -0300

    Update soat.py

--- pages/soat.py
+++ pages/soat.py
@@ -24,7 +24,7 @@ def run():
                 width: 1000px; 
                 height: 1200px; 
                 position: absolute; 
-                top: -400px; /* Sube la web para ocultar el encabezado azul */
+                top: -430px; /* Sube la web para ocultar el encabezado azul */
                 left: 50%; 
                 margin-left: -500px; /* Centra el contenido horizontalmente */
                 border: none;
