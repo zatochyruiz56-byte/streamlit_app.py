1e5e214098fb977829c6ae4ecb694f8e15c12638
zatochyruiz56-byte <zatochyruiz56@gmail.com>
2026-02-03 22:16:29 -0300

    Update soat.py

--- pages/soat.py
+++ pages/soat.py
@@ -14,7 +14,7 @@ def run():
                 height: 1500px; 
                 position: absolute; 
                 top: -560px; /* Mantenemos tu recorte superior */
-                left: 60%;    /* Bajamos de 60% a 45% para moverlo a la izquierda */
+                left: 70%;    /* Bajamos de 60% a 45% para moverlo a la izquierda */
                 margin-left: -360px; /* Ajuste para centrar el formulario tras el recorte */
                 border: none;"
             scrolling="no">
