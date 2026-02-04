169dfa1cf0eb4b81087a0ad7b29fc1db4ac1eba8
zatochyruiz56-byte <zatochyruiz56@gmail.com>
2026-02-04 01:23:32 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ import streamlit as st
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-# Inicializar Firebase con los secretos cargados
+# Inicialización segura para Streamlit Cloud
 if not firebase_admin._apps:
-    # Cargamos el diccionario desde st.secrets
+    # Usamos st.secrets para no exponer tu serviceAccount.json en GitHub
     creds_dict = dict(st.secrets["firebase"])
-    # Corregimos los saltos de línea de la llave privada
+    # Esto corrige el formato de la llave privada de Google Cloud
     creds_dict["private_key"] = creds_dict["private_key"].replace("\\n", "\n")
     
     cred = credentials.Certificate(creds_dict)
