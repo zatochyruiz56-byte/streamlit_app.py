9edf6ed62c903a29308b55b36043201ff1a1336e
zatochyruiz56-byte <zatochyruiz56@gmail.com>
2026-02-03 21:02:17 -0300

    Update soat.py

--- pages/soat.py
+++ pages/soat.py
@@ -26,7 +26,7 @@ def run():
     html_pacifico = """
     <div style="width: 100%; height: 480px; overflow: hidden; border: 2px solid #0096d2; border-radius: 12px; position: relative; background: white;">
         <iframe src="https://api.allorigins.win/raw?url=https://www.pacifico.com.pe/consulta-soat" 
-            style="width: 1000px; height: 1500px; position: absolute; top: -150px; left: 80%; margin-left: -750px; border: none;"
+            style="width: 1000px; height: 1500px; position: absolute; top: -150px; left: 80%; margin-left: -650px; border: none;"
             scrolling="no"></iframe>
     </div>
     """
