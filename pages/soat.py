9c4f5f14a2de130b223d91ad58bfa1c7fedd7b37
zatochyruiz56-byte <zatochyruiz56@gmail.com>
2026-02-03 22:06:16 -0300

    Update soat.py

--- pages/soat.py
+++ pages/soat.py
@@ -15,7 +15,7 @@ def run():
                 height: 1500px; 
                 position: absolute; 
                 top: -560px; 
-                left: 60%; 
+                left: 70%; 
                 margin-left: -400px; 
                 border: none;"
             scrolling="no">
